bd474e3ad820ecb3a243581840c8c389e42bd60a
Lois Clinton <loisclinton@hotmail.co.uk>
2025-10-14 13:54:47 +0100

    feat(player): add individual x and y direction for better animation

--- Player.gd
+++ Player.gd
@@ -1,17 +1,18 @@
 extends CharacterBody2D
+@onready var animated_player_sprite = $AnimatedSprite2D
 
 const SPEED: float = 100.0
-
 func _physics_process(delta: float) -> void:
-	# Get the input direction and handle the movementn.
-	# Good practice to replace UI actions with custom actions.
-	var direction = Vector2(
-		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
-		Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
-	)
 	
-	print(direction)
+	# Get input direction and handle movement.
+	var direction_x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	var direction_y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	var direction = Vector2(direction_x,direction_y)
+	
+	# FOR DEBUGGING
+	# print(direction)
+	# print(direction_x)
+	# print(direction_y)
 	velocity = direction * SPEED
 	
-
 	move_and_slide()

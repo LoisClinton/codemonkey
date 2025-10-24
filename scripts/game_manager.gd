084346588964ed54caf013aed70bd6ad2ef39c21
Lois Clinton <loisclinton@hotmail.co.uk>
2025-10-24 09:03:03 +0100

    feat: add set player function to game manager

---
+++ scripts/game_manager.gd
@@ -0,0 +1,24 @@
+extends Node
+
+@onready var bug_score = 0
+@onready var user_story_score = 0
+@onready var bug_label = $"Player/Camera2D/BugLabel"
+@onready var user_story_label = $"Player/Camera2D/UserStoryLabel"
+
+
+var player_node:Node = null
+
+func add_collectible():
+	bug_label.text = "Bugs: " + str(bug_score) + "/10"
+	user_story_label.text = "User Stories: " + str(user_story_score) +"/10"
+	
+func add_bug():
+	bug_score += 1
+	add_collectible()
+	
+func add_user_story():
+	user_story_score += 1
+	add_collectible()
+
+func set_player(player):
+	player_node = player 

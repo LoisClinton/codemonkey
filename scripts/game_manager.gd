78ca51cda7772bc00372a542003d1218cb9786c8
Lois Clinton <loisclinton@hotmail.co.uk>
2025-10-28 19:10:19 +0000

    refactor(game_manager.gd): tidy up logic inside game manager, use call_deferred() to change scene tree for best practice, edit comments

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,40 +1,54 @@
 class_name GameManager extends Node
 
-const TARGET := 10
-var player_node: Node
-var bug_label_node: Label 
-var user_story_label_node: Label 
+#variables
+const TARGET: int = 10
 const WIN_GAME_SCENE_PATH: String = "res://scenes/win_game_screen.tscn"
-@onready var _bug_score = 0
-@onready var _user_story_score = 0
+var player_node: Node
+var bug_label_node: Label
+var user_story_label_node: Label
+var _bug_score: int = 0
+var _user_story_score: int = 0
+var _has_won: bool = false
 
-func set_player(player: Node):
-	player_node = player 
+#getters & setters
+func set_player(player: Node) -> void:
+	player_node = player
 
-func get_bug_score() -> int: return _bug_score
-func get_user_story_score() -> int: return _user_story_score
-func set_bug_label(bug_label: Label)-> void: bug_label_node = bug_label
-func set_user_story_label(user_story_label: Label)-> void: user_story_label_node = user_story_label
+func set_bug_label(bug_label: Label) -> void:
+	bug_label_node = bug_label
 
-func add_collectible():
-	if bug_label_node:
-		bug_label_node.text = "Bugs: " + str(_bug_score) + "/10"
-	if user_story_label_node:
-		user_story_label_node.text = "User Stories: " + str(_user_story_score) +"/10"
+func set_user_story_label(user_story_label: Label) -> void:
+	user_story_label_node = user_story_label
 
-func add_bug():
+func get_bug_score() -> int:
+	return _bug_score
+
+func get_user_story_score() -> int:
+	return _user_story_score
+
+#mutators
+func add_bug() -> void:
 	if _bug_score < TARGET:
 		_bug_score += 1
-		add_collectible()
+		_update_hud_and_check()
+
+func add_user_story() -> void:
+	if _user_story_score < TARGET:
+		_user_story_score += 1
+		_update_hud_and_check()
+
+#internals
+func _update_hud_and_check() -> void:
+	if is_instance_valid(bug_label_node):
+		bug_label_node.text = "Bugs: %d/%d" % [_bug_score, TARGET]
+	if is_instance_valid(user_story_label_node):
+		user_story_label_node.text = "User Stories: %d/%d" % [_user_story_score, TARGET]
 	_check_win()
 
-func add_user_story():
-	if _user_story_score  < TARGET:
-		_user_story_score += 1
-		add_collectible()
-	_check_win()
-	
 func _check_win() -> void:
+	if _has_won:
+		return
 	if _bug_score >= TARGET and _user_story_score >= TARGET:
+		_has_won = true
 		print("YOU WIN")
-		get_tree().change_scene_to_file(WIN_GAME_SCENE_PATH)
+		get_tree().call_deferred("change_scene_to_file", WIN_GAME_SCENE_PATH)

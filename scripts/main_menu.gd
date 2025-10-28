b8685dc1a56b16e5494fd491e153caea8e0eeaf9
Lois Clinton <loisclinton@hotmail.co.uk>
2025-10-28 19:45:31 +0000

    refactor(main_menu.gd): tidy up logic inside main_menu, make main menu reusable across win game scene and main menu scenes, with buttons being optional

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,17 +1,23 @@
 class_name MainMenu extends Node
 
+var play_btn: Button
+var exit_btn: Button
 const GAME_SCENE_PATH: String = "res://scenes/game.tscn"
-@onready var play_btn: Button = $PlayButton
-@onready var exit_btn: Button = $ExitButton
 
 func _ready() -> void:
-	play_btn.pressed.connect(_on_play_button_pressed)
-	exit_btn.pressed.connect(_on_exit_button_pressed)
+	# find these nodes only if they exist in the scene
+	play_btn = get_node_or_null("PlayButton") as Button
+	exit_btn = get_node_or_null("ExitButton") as Button
+
+	if play_btn:
+		play_btn.pressed.connect(_on_play_button_pressed)
+	if exit_btn:
+		exit_btn.pressed.connect(_on_exit_button_pressed)
 
 func _on_play_button_pressed() -> void:
 	var ok := get_tree().change_scene_to_file(GAME_SCENE_PATH)
 	if ok != OK:
-		push_error("Failed to start game scene: %s" % GAME_SCENE_PATH)
+		print("Failed to start game scene: %s" % GAME_SCENE_PATH)  # added for testing
 	else:
 		print("Game started successfully") # added for testing
 
--- scripts/win_game_screen.gd
+++
@@ -1,4 +0,0 @@
-class_name WinScreen extends MainMenu
-
-func _ready() -> void:
-	exit_btn.pressed.connect(_on_exit_button_pressed)
